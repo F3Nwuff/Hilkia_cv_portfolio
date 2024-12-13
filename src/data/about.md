9f07c3a7c8a2448898fcd8071a820209e35226f0
F3Nwuff <114073235+F3Nwuff@users.noreply.github.com>
2024-12-13 19:28:36 +0700

    Add files via upload

---
+++ src/data/about.md
@@ -0,0 +1,35 @@
+# Intro
+
+I am currently a student for Computer Science in [BINUS International University](https://international.binus.ac.id/), i am expected to graduate by 2026.
+at this current state in time im passionate about data analysis and game development. currently i am developing and AI to detect malware in Android phones and a hidden maze game.
+
+in my spare time i like to cook, hang out with friends, reading and random simple activities that improve my understanding and capabilities in random minor ways.
+
+# personal interests
+
+- cooking and baking
+- reading random interesting books
+- soccer or futsal
+- mythologies and legends
+- solving simple conflict questions or issues
+- random new activities
+- making simple old school games
+- playing chess every so often
+- activities with friends
+- games that benefits from game understanding or knowledge
+
+# Fun Facts
+
+- I have created many personal recipies on a variety of cuisines and techniques.
+- I am able to tell multiple stories of certain locations myth and legends as well as opinions surrounding them and their meanings.
+- in most activities i am constantly in a supporting or defending position and can actually do quite well.
+- I have consistently been able to do well in all team or social activities, even with a wide variety of diverse or changing participants.
+- I have consistently been selected and participated in olympics for 6 years straight.
+- my favorite games are ones that benefits from strategy, mechanical understanding and teamwork.
+
+# I Dream Of
+
+- gaining financial stability 
+- making a good environment for those around me
+- Working on a project that positively impacts the community by creating gamified apps for mental and cognitive training
+- identifying patterns and trends that improves general effectiveness and efficiency of myself and those around me
---
+++ src/data/projects.js
@@ -0,0 +1,132 @@
+// TODO Add a couple lines about each project
+const data = [
+  {
+    title: 'nero',
+    subtitle: '[Human Computer Interaction]',
+    link: 'https://github.com/F3Nwuff/nero',
+    image: '/images/nero.png',
+    date: '2023-01-20',
+    desc:
+     'Nero is a personal hydration assistant that helps users monitor their water '
+     + 'intake and provides reminders to stay hydrated throughout the day. Developed as a final project '
+     + 'for a Human-Computer Interaction course, it aims to make tracking water consumption simple and convenient, '
+     + 'assisting users in achieving their daily hydration goals and improving overall health.',
+  },
+  {
+    title: ' Board Game Compilation',
+    subtitle: '[Algorithm and Programming]',
+    link: 'https://github.com/F3Nwuff/final-project-algorithm-and-programing-1st-semester_Hilkia-Kennan-Latjandu_2602174485',
+    image: '/images/board_game.png',
+    date: '2023-01-16',
+    desc:
+     'create a python based compilation of simple multiplayer board games with small twists with an older school user interface.',
+  },
+  {
+    title: 'FSM - Car Turn Signal',
+    subtitle: '[Scientific Computing]',
+    link: 'https://github.com/F3Nwuff/scicomp-fsm',
+    image: '/images/fsm.png',
+    date: '2023-01-24',
+    desc:
+     'This project is a simple finite state machine (FSM) that controls a cars turn signals using two buttons. '
+     + 'The system has four possible states: both lights off, left light on, right light on, and both lights on.'
+     + 'Visual aids like state diagrams, truth tables, and Karnaugh maps are included to illustrate the FSMs design '
+     + 'and logic.',
+  },
+  {
+    title: 'word games compilation',
+    subtitle: '[Object Oriented Programming]',
+    link: 'https://github.com/F3Nwuff/OOPFinalProjectSem2',
+    image: '/images/word_game.png',
+    date: '2023-01-17',
+    desc:
+     'create a java based compilation of word games which are hangman, scramble, unscramble and wordle,'
+     + ' with a more simplistic and old school user interface, this compilation also has small twists.',
+  },
+  {
+    title: 'tsunami prediction system',
+    subtitle: '[Fundamentals of Data Science]',
+    link: 'https://github.com/F3Nwuff/DataScience-BasicStatistics-Final-Project',
+    image: '/images/ds.png',
+    date: '2024-01-23',
+    desc:
+     'This project evaluates various supervised machine learning algorithms, such as Support Vector Machine, '
+     + 'Random Forest, Decision Tree, XGBoost, Gradient Boosting, and Logistic Regression, to identify the most '
+     + 'effective model for predicting tsunamis using a dataset sourced from Kaggle.',
+  },
+  {
+    title: 'shop stocks',
+    subtitle: '[Database Technology]',
+    link: 'https://github.com/F3Nwuff/database',
+    image: '/images/db.png',
+    date: '2024-01-4',
+    desc:
+     'using a Python script that interacts with a SQL database for tasks such as data retrieval, insertion, '
+     + 'or management. It is useful for database management and store stock management.',
+  },
+  {
+    title: 'DFS and BFS showcased in python maze',
+    subtitle: '[Algorithm Design and Analysis]',
+    link: 'https://github.com/F3Nwuff/sem3-ADA-Final-Project',
+    image: '/images/ada.png',
+    date: '2024-01-08',
+    desc:
+     'This project demonstrates the implementation of Depth-First Search (DFS) and Breadth-First Search (BFS) '
+     + 'algorithms using Python in a python maze. It includes scripts that showcase these algorithms in action, '
+     + 'providing practical examples of their application.',
+  },
+  {
+    title: 'physics car game',
+    subtitle: '[Computational Physics]',
+    link: 'https://github.com/F3Nwuff/car_game_physics',
+    image: '/images/phys.png',
+    date: '2024-06-18',
+    desc:
+     'This project is a Python and Pygame-based simulation that models how different terrains affect car movement, '
+     + 'focusing on factors like friction, traction, and momentum. It features a simplified map inspired by Mario Circuit 1 '
+     + 'and includes six vehicle presets commonly used in Indonesia and China.',
+  },
+  {
+    title: 'to-do list',
+    subtitle: '[Web App Development and Security]',
+    link: 'https://github.com/F3Nwuff/sqltodosesionandcookie',
+    image: '/images/todo.png',
+    date: '2024-05-14',
+    desc:
+     'This project is a to-do list application built with React and Vite, utilizing Firebase for data storage and user '
+     + 'authentication. It manages user sessions and cookies to maintain state across sessions, enabling users to add, '
+     + 'edit, and delete tasks while ensuring data persistence and security.',
+  },
+  {
+    title: 'MorAlm',
+    subtitle: '[Web App Development and Security]',
+    link: 'https://github.com/orgs/WADSFinalProject/repositories',
+    image: '/images/moralm.png',
+    date: '2024-06-28',
+    desc:
+      'MorAlm is a web application designed to optimize the supply chain management of Moringa leaves, replacing '
+      + 'inefficient manual processes with a digital solution. It tracks and manages production, transportation, and '
+      + 'inventory across sectors like Centra (processing), Harbor (logistics), and MorAlm office (monitoring and '
+      + 'packaging). The app provides role-based access, real-time notifications, and a user-friendly interface for '
+      + 'efficient operations. Built with MySQL, Vercel, and Firebase, MorAlm supports both web and mobile platforms to '
+      + 'ensure accessibility and convenience for all users. '
+      + 'below are the live links',
+  },
+  {
+    title: 'xyz',
+    link: 'https://xyz-moralm.vercel.app/',
+    image: '/images/moralm.png',
+  },
+  {
+    title: 'harbor',
+    link: 'https://harbormoralm.vercel.app/',
+    image: '/images/moralm.png',
+  },
+  {
+    title: 'centra',
+    link: 'https://centramoralm.vercel.app/',
+    image: '/images/moralm.png',
+  },
+];
+
+export default data;
