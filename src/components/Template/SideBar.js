2f059d1d72355e2559169566c3eba1f8392609c6
F3Nwuff <114073235+F3Nwuff@users.noreply.github.com>
2024-12-13 19:27:01 +0700

    Add files via upload

---
+++ src/components/Template/SideBar.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import ContactIcons from '../Contact/ContactIcons';
+
+const { PUBLIC_URL } = process.env; // set automatically from package.json:homepage
+
+const SideBar = () => (
+  <section id="sidebar">
+    <section id="intro">
+      <Link to="/" className="logo">
+        <img src={`${PUBLIC_URL}/images/me1.jpg`} alt="" />
+      </Link>
+      <header>
+        <h2>Hilkia Kennan Latjandu</h2>
+        <p>
+          <a href="mailto:hilkiakennan@gmail.com">hilkiakennan@gmail.com</a>
+        </p>
+      </header>
+    </section>
+
+    <section className="blurb">
+      <h2>About</h2>
+      <p>
+        Hello, I&apos;m Kennan. I am currently a student of {' '}
+        <a href="https://international.binus.ac.id/">BNUS International University</a>,
+        i will be graduating by the year 2026
+      </p>
+      <ul className="actions">
+        <li>
+          {!window.location.pathname.includes('/resume') ? (
+            <Link to="/resume" className="button">
+              Learn More
+            </Link>
+          ) : (
+            <Link to="/about" className="button">
+              About Me
+            </Link>
+          )}
+        </li>
+      </ul>
+    </section>
+
+    <section id="footer">
+      <ContactIcons />
+      <p className="copyright">
+        &copy; Michael D&apos;Angelo <Link to="/">mldangelo.com</Link>.
+      </p>
+    </section>
+  </section>
+);
+
+export default SideBar;
